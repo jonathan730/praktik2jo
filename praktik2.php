71f2f8dfb2e854d76d9b3e649a4b4598fe95a2ff
jonathan730 <jcalvyano@gmail.com>
2019-09-20 10:50:38 +0700

    github.io

---
+++ praktik2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Praktik 2</title>
+</head>
+<body>
+<h1 style="color: red">WEB INI UJI COBA UNTUK CLOUD DI PRAKTIK 2 SEKARANG</h1>
+<p> WEB INI UJI COBA UNTUK CLOUD DI PRAKTIK 2 SEKARANG</p>
+<p>Jonathan Calvyano 175410094</p>
+<img src="download.jpg">
+</body>
+</html>
